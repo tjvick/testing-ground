a6a7fe1401c511b7977b499e8063fa4bd2a15d16
Tyler Vick <vick.tyler@gmail.com>
2018-09-26 22:03:17 -0400

    Build vue app for groups

--- scripts/photo-collage.js
+++ scripts/photo-collage.js
@@ -1,16 +1,24 @@
+const { Siema } = require('./_util/siema.js');
+
 
 const functionURL = window.location.hostname === 'localhost' ? 'http://localhost:9000' : '/.netlify/functions';
 
-
-function showImg(person) {
-  console.log(person);
-  console.log(person.imageUrl);
-  console.log(person.imageUrl === undefined);
-  return person.imageUrl !== undefined;
+function isBlank(property) {
+  return (
+    property === undefined ||
+    property === '' ||
+    property === ['']
+  );
 }
+
+const showImg = person => !isBlank(person.imageUrl);
+const showGroupName = group => !isBlank(group.groupName);
+const showLocation = person => !isBlank(person.location);
+const showImgGroup = group => !isBlank(group.imageUrls);
 
 const appData = {
   clubData: [],
+  checked: false,
 };
 
 const app = new Vue({
@@ -20,7 +28,24 @@ const app = new Vue({
   methods: {
     showImg,
   },
-})
+});
+
+const app2Data = {
+  clubData: [],
+  checked: true,
+};
+
+const app2 = new Vue({
+  delimiters: ['${', '}'],
+  el: '#vue-app-2',
+  data: app2Data,
+  methods: {
+    showGroupName,
+    showLocation,
+    showImgGroup,
+  },
+});
+
 
 
 fetch(`${functionURL}/getData`, {
@@ -28,7 +53,22 @@ fetch(`${functionURL}/getData`, {
 })
 .then(res => res.json())
 .then(res => {
-  console.log(res);
   appData.clubData = res;
+})
+.catch(error => console.log('Error:', error));
+
+
+fetch(`${functionURL}/getDataGrouped`, {
+  method: 'GET',
+})
+.then(res => res.json())
+.then(res => {
+  app2Data.clubData = res;
+})
+.then(res => {
+  new Siema({
+    selector: '.u-siema',
+    loop: true,
+  });
 })
 .catch(error => console.log('Error:', error));
