9396a9ad68145cdda2d2d4848d1987332cbc4c93
Tyler Vick <vick.tyler@gmail.com>
2018-09-28 20:24:51 -0400

    Clean up carousel page js

--- scripts/page-carousel.js
+++ scripts/page-carousel.js
@@ -2,12 +2,5 @@ require('./components/photo-display.js');
 
 const app = new Vue({
   el: '#app',
-  data() {
-    return {
-      imageUrls: [
-        '/media/image2-2000px.jpg',
-        // '/media/image1-2000px.jpg',
-      ],
-    };
-  },
+  data: {},
 });
