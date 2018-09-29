93620b633f1e64b91a91b5571bc6b4ab55a0a0d0
Tyler Vick <vick.tyler@gmail.com>
2018-09-28 20:11:35 -0400

    Handle lack of images in photo-display

--- scripts/components/photo-display.js
+++ scripts/components/photo-display.js
@@ -4,11 +4,11 @@ require('./photo-carousel.js');
 
 Vue.component('photo-display', {
   template: `
-    <div class="c-photo-display">
+    <div class="c-photo-display" v-if="showAnything">
       <photo-carousel v-if="showCarousel" v-bind:image-urls="imageUrls"></photo-carousel>
       <div class="c-not-carousel">
         <div class="c-carousel__image-container" v-if="!showCarousel">
-        <img class="c-carousel__image" v-bind:src="imageUrls[0]" alt="">
+          <img class="c-carousel__image" v-bind:src="imageUrls[0]" alt="">
         </div>
       </div>
     </div>
@@ -18,12 +18,19 @@ Vue.component('photo-display', {
   },
   computed: {
     showCarousel() {
-      if (this.imageUrls.length > 1) {
+      if (this.showAnything && this.imageUrls.length > 1) {
         return true;
       }
 
       return false;
     },
+    showAnything() {
+      if (this.imageUrls === undefined) {
+        return false;
+      }
+
+      return true;
+    }
   },
   methods: {},
   props: ['imageUrls'],
